f150e3fd221e3c024d0ee56bc6d4a3a70fb613d6
Heshie London <heshielondon@gmail.com>
2018-06-23 15:40:26 -0700

    Add SharedDetail components

---
+++ client/src/components/Shared/DetailStats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DetailStats extends React.Component {
+  render() {
+    return (
+      <div>
+        FILL_ME_IN
+      </div>
+    );
+  }
+}
+
+export default DetailStats;
---
+++ client/src/components/Shared/SharedUserProfileBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserProfileBox extends React.Component {
+  render() {
+    return (
+      <div>
+        FILL_ME_IN
+      </div>
+    );
+  }
+}
+
+export default UserProfileBox;
