021ec122b6b1bf052ea59960ae08df7c4c30c714
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-06-23 14:03:21 -0700

    Add menu components

---
+++ client/src/components/Menu/SchoolMenu/SchoolMenuHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SchoolMenuHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        FILL_ME_IN
+      </div>
+    );
+  }
+}
+
+export default SchoolMenuHeader;
