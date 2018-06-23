2189a72e2fccb5560636b3b71edcd1a563535646
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-06-23 00:39:55 -0700

    Refactor component template to remove excess code

--- client/src/components/Admin/AdminLanding/AdminLandingCharts.jsx
+++ client/src/components/Admin/AdminLanding/AdminLandingCharts.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import FILL_ME_IN from 'FILL_ME_IN';
 
 class FILL_ME_IN extends React.Component {
-
   render() {
     return (
       FILL_ME_IN
--- client/src/components/Menu/Footer.jsx
+++ client/src/components/Menu/Footer.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import FILL_ME_IN from 'FILL_ME_IN';
 
 class FILL_ME_IN extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       FILL_ME_IN
