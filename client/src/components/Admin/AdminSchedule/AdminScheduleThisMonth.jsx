a3157c57efd1b9758cd19f509e75d46c7d8046ec
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-06-22 19:14:16 -0700

    Add React components and component template in each

---
+++ client/src/components/Admin/AdminSchedule/AdminScheduleThisMonth.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FILL_ME_IN from 'FILL_ME_IN';
+
+class FILL_ME_IN extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      FILL_ME_IN
+    );
+  }
+}
+
+export default FILL_ME_IN;
