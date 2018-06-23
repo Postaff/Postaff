55296865a152d07420c23da9fd72b759f4238760
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-06-23 11:55:43 -0700

    Organize school and sub files into subfolders

---
+++ client/src/components/School/SchoolJobDetailView/SchoolJobDetailViewMenuBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SchoolJobDetailViewMenuBar extends React.Component {
+  render() {
+    return (
+      <div>
+        FILL_ME_IN
+      </div>
+    );
+  }
+}
+
+export default SchoolJobDetailViewMenuBar;
---
+++ client/src/components/School/SchoolJobDetailView/SchoolJobDetailViewOverview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SchoolJobDetailViewOverview extends React.Component {
+  render() {
+    return (
+      <div>
+        FILL_ME_IN
+      </div>
+    );
+  }
+}
+
+export default SchoolJobDetailViewOverview;
