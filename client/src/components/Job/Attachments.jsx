0897e6b4c9546bf146920c9a9c9f79b18b178195
Heshie London <heshielondon@gmail.com>
2018-06-26 16:49:00 -0700

    Add update button with file input

--- client/src/components/Job/Attachments.jsx
+++ client/src/components/Job/Attachments.jsx
@@ -1,29 +1,59 @@
 import React from 'react';
 import {
   CardContent, 
-  Typography, 
-  CardMedia, 
-  Card,
+  Typography,
+  Paper,
 } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import sampleData from './sampleData.js';
+import FileUpload from '@material-ui/icons/FileUpload';
 
-class Attachments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pageInfo: sampleData
-    }
-  }
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+  input: {
+    display: 'none',
+  },
+});
 
-  render() {
-    return (
-      
-      <Card style={{ flex: 1, height: 200, marginTop: 20, marginLeft: 50, marginRight:50}}>
+function Attachments(props) {
+  const { classes } = props;
+  return (
+    <Paper style={{ height: '100%' }}>
+    <div>
+      <Typography variant="display1" gutterBottom>
         Attachments
-      </Card>
-     
-    );
-  }
+      </Typography> 
+      <input
+      accept="image/*"
+      className={classes.input}
+      id="flat-button-file"
+      multiple
+      type="file"
+      />
+      <label htmlFor="flat-button-file">
+        <Button component="span" color="primary" className={classes.button}>
+         Upload
+         <FileUpload className={classes.rightIcon} />
+        </Button>
+      </label>
+      </div>
+    </Paper>
+  );
 }
 
-export default Attachments;
+Attachments.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(Attachments);
