a1b083c9c6b8ce8dad0c103ac18d2055c40aed3f
Heshie London <heshielondon@gmail.com>
2018-06-26 16:49:37 -0700

    Improve reusability and add grid for layout

---
+++ client/src/components/Job/Detail.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  CardContent,
+  Typography,
+  Paper,
+  Grid,
+} from '@material-ui/core';
+import sampleData from './sampleData.js';
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageInfo: sampleData
+    }
+  }
+
+  render() {
+    return (
+      <Paper style={{ height: '100%' }}>
+       <Typography variant="display1" gutterBottom>
+         Detail
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              Date Needed:
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={5}>
+              { this.state.pageInfo.datesNeeded}
+            </Grid>
+          </Grid>
+        </Typography>  
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              Status:
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={5}>
+              { this.state.pageInfo.status}
+            </Grid>
+          </Grid>
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              Subjects:
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={5}>
+              { this.state.pageInfo.subjects}
+            </Grid>
+          </Grid>
+        </Typography>
+                  <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              Grade:
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={5}>
+              { this.state.pageInfo.grade}
+            </Grid>
+          </Grid>
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={2}>
+              Hours Availaible:
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={5}>
+              { this.state.pageInfo.hours}
+            </Grid>
+          </Grid>
+        </Typography>
+      </Paper>
+    );
+  }
+}
+
+export default Detail;
