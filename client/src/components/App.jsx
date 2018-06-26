6485ca746ad14e5d45395425698a9edd33f2eb20
Mayank Patel <patelpmayank20@gmail.com>
2018-06-25 23:20:25 -0500

    adds routing to job detail component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import HomeLanding from "./Home/HomeLanding.jsx";
@@ -29,27 +28,6 @@ class App extends React.Component {
     );
   }
   
-=======
-import React, {Component} from "react";
-import { BrowserRouter, Route} from "react-router-dom";
-import HomeLanding from "./Home/HomeLanding.jsx";
-import LoginLanding from "./Login/LoginLanding.jsx";
-// import AdminLandingOverview from "./Admin/AdminLanding/AdminLandingOverview.jsx";
-// import AdminScheduleOverview from "./Admin/AdminSchedule/AdminScheduleOverview.jsx"
-const App = () => {
-  return (
-    
-    <BrowserRouter> 
-      <div>
-        <Route exact path="/" component={HomeLanding} /> 
-        <Route path="/login" component={LoginLanding} />
-        {/* <Route path="/admin" component={AdminLandingOverview} />
-        <Route path="/admin/schedule" component={AdminScheduleOverview} /> */}
-      </div>  
-    </BrowserRouter>  
-   
-  );
->>>>>>> adds routing to homeLanding and LoginLanding view
 };
 
 export default App;
--- client/src/components/Sub/SubJobDetail/SubJobDetail.jsx
+++ client/src/components/Sub/SubJobDetail/SubJobDetail.jsx
@@ -9,16 +9,11 @@ class LoginLanding extends React.Component {
     console.log("Hey am in login landing page")
     return (
       <div>
-<<<<<<< HEAD
         <ul>
           <li><Link to="/admin">Admin Log in</Link></li>
           <li><Link to="/school">School Log in</Link></li>
           <li><Link to="/sub">Sub Log in</Link></li>
         </ul>  
-=======
-       <p>USERNAME</p>
-       <p>PASSWORD</p>
->>>>>>> adds routing to homeLanding and LoginLanding view
       </div>
     );
   }
