7719d2d122150b597a724c4f8b506c9788b9f5b5
Heshie London <heshielondon@gmail.com>
2018-06-27 11:17:23 -0700

    Refactor admin dash components with material

--- client/src/components/Admin/AdminLanding/AdminLandingUnclaimedJobsList.jsx
+++ client/src/components/Admin/AdminLanding/AdminLandingUnclaimedJobsList.jsx
@@ -1,33 +1,49 @@
+// class AdminLandingUnclaimedJobsList extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       entries: ['Unclaimed Job 1', 'Unclaimed Job 2']
+//     }
+//   }
+//             {this.state.entries.map((entry, index) => <AdminLandingUnclaimedJobsEntry entry={entry} key={index} />)}
+
 import React from 'react';
-import AdminLandingUnclaimedJobsEntry from './AdminLandingUnclaimedJobsEntry.jsx';
+import {
+  Typography,
+  Paper,
+  Grid,
+} from '@material-ui/core';
 
 class AdminLandingUnclaimedJobsList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      entries: ['Unclaimed Job 1', 'Unclaimed Job 2']
-    }
-  }
-
   render() {
     return (
-      <div>
-        <h3>Unclaimed Jobs</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Job</th>
-              <th>Start Date</th>
-              <th>Start Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.entries.map((entry, index) => <AdminLandingUnclaimedJobsEntry entry={entry} key={index} />)}
-          </tbody>
-        </table>
-      </div>
-    )
+      <Paper style={{ height: '100%', padding: '5%' }}>
+        <Typography variant="display1" gutterBottom>
+         Unclaimed Jobs
+        </Typography>
+        <Typography variant="title" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={4}>
+              Job
+            </Grid>
+            <Grid item xs={4}>
+              Date
+            </Grid>
+          </Grid>
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={4}>
+              PE / General Ed
+            </Grid>
+            <Grid item xs={4}>
+              07/06/18 8:55am
+            </Grid>
+          </Grid>
+        </Typography>
+      </Paper>
+    );
   }
 }
 
