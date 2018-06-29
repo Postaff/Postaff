f513ed1f6fb5a767e9497d2b877be437a0eb0586
Heshie London <heshielondon@gmail.com>
2018-06-28 19:19:20 -0700

    Update package.json

--- client/src/components/Admin/AdminLanding/AdminLandingCharts.jsx
+++ client/src/components/Admin/AdminLanding/AdminLandingCharts.jsx
@@ -3,11 +3,33 @@ import React from 'react';
 class AdminLandingCharts extends React.Component {
   render() {
     return (
-      <div>
-        FILL_ME_IN
-      </div>
-    );
-  }
+      <Paper style={{ height: '100%', padding: '5%' }}>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              Stats
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={1}>
+              27
+            </Grid>
+          </Grid>
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              Stats
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={1}>
+              144
+            </Grid>
+          </Grid>
+        </Typography>
+      </Paper>
+    );  }
 }
 
 export default AdminLandingCharts;
