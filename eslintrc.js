39183e068695ac1816fdc1ed5e92ae8a7838efea
anotherFeng <regnagleppod@gmail.com>
2018-06-28 22:55:49 -0400

    add eslint

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+      "ecmaVersion": 6,
+      "ecmaFeatures": {
+          "jsx": true
+      }
+  }
+};
