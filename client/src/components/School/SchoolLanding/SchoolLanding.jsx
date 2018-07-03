c1ab14b16afa7fa3794aae1d3d285b489a785465
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-07-02 23:48:25 -0700

    Add jobs table to school module dashboard

--- client/src/components/School/SchoolLanding/SchoolLanding.jsx
+++ client/src/components/School/SchoolLanding/SchoolLanding.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import {Grid} from '@material-ui/core';
+import SchoolLandingJobsTable from './SchoolLandingJobsTable.jsx';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 class SchoolLanding extends React.Component {
   render() {
     return (
-      <div>
-        <h1>this is school landing page</h1>
-      </div>
+      <Fragment>
+        <Grid container spacing={24}>
+          <SchoolLandingJobsTable />
+        </Grid>
+      </Fragment>
     );
   }
 }
 
-export default SchoolLanding;
+export default withStyles(styles)(SchoolLanding);
