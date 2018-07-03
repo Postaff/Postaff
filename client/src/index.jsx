81dde86c3b1b53e4651c6697cc571a2e753c386d
Heshie London <heshielondon@gmail.com>
2018-07-03 15:52:18 -0700

    Add global app padding

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -12,36 +12,35 @@ import reducers from './reducers/indexReducer.js';
  * Create a new instant of ApolloClient and apply cache middleware
  * dataIdFromObject is a function that takes a data object and return
  * a unique identifier to be used when normalizing the data in the store.
- * 
+ *
  * I would suggest this read if you're interest at reading more
  * https://www.apollographql.com/docs/react/advanced/caching.html#normalization
  */
 const client = new ApolloClient({
-  link: new HttpLink({uri: 'http://localhost:3000/graphql'}),
+  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
   dataIdFromObject: o => o.id,
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 const store = createStore(
   reducers,
-  { 
-    auth: { authenticated: localStorage.getItem('token')}
+  {
+    auth: { authenticated: localStorage.getItem('token') },
   },
-  applyMiddleware(reduxThunk)
-)
+  applyMiddleware(reduxThunk),
+);
 
-const Root = () => {
-  return (
-    <ApolloProvider client={ client }>
-      <ReduxProvider store={ store }>
-        <Fragment>
-          <App/>
-        </Fragment>
-      </ReduxProvider>
-    </ApolloProvider>
-  )
-};
+const Root = () => (
+  <ApolloProvider client={ client }>
+    <ReduxProvider store={ store }>
+      <Fragment>
+        <App />
+      </Fragment>
+    </ReduxProvider>
+  </ApolloProvider>
+);
 
 ReactDOM.render(
-  <Root />, 
-document.getElementById("app"));
+  <Root />,
+  document.getElementById('app'),
+);
