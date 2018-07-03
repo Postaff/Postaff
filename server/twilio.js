d7bcc735312e9cfc8637a540c248a82d91a6dcc9
anotherFeng <regnagleppod@gmail.com>
2018-07-03 15:04:04 -0400

    add twilio

--- server/twilio.js
+++ server/twilio.js
@@ -2,3 +2,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+  to: ''
+})
