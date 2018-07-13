541c852c94d5f4034e4eb8dd851c638c0873fee1
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-07-12 21:43:05 -0700

    Revert let to var

--- database/indexDb.js
+++ database/indexDb.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 
-let sequelize = null;
+var sequelize = null;
 
-if(process.env.CLEARDB_DATABASE_URL) {
-  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
-    dialect: 'mysql',
-    host: 'us-cdbr-iron-east-04.cleardb.net',
-  });
-} else {
+// if (process.env.CLEARDB_DATABASE_URL) {
+//   sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+//     dialect: 'mysql',
+//     host: 'us-cdbr-iron-east-04.cleardb.net',
+//   });
+// } else {
   sequelize = new Sequelize('postaff', process.env.SQL_DBUSER, process.env.SQL_DBPASS, {
     dialect: 'mysql',
     host: 'localhost',
   });
-}
+// }
 
 sequelize
   .authenticate()
